0bf2e3e568b79169c67826dd73cf492bed6f97da
Ujjwal Singh <ujjwalks01@gmail.com>
2020-03-09 00:20:06 +0530

    add support to read filenames in  directory and some basic json methods

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="file-utility-al", # Replace with your own username
+    name="file-utility-al",
     version="0.0.1",
     author="Ujjwal Singh",
     author_email="ujjwalks01@gmail.com",
